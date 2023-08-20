b9d94b87bb2583ab7ea78c7b2d29eab39ae24e03
Christian Mugione <chmugione@gmail.com>
2023-08-20 10:00:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000; // Puedes cambiar el puerto si lo deseas
+
+// Ruta que devuelve un array de objetos en formato JSON
+app.get("/api/objetos", (req, res) => {
+  const objetos = [
+    { id: 1, nombre: "Objeto 1" },
+    { id: 2, nombre: "Objeto 2" },
+    { id: 3, nombre: "Objeto 3" },
+  ];
+
+  res.json(objetos);
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
