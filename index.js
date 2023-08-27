eb88b44b4a773a3746c351cc39fb2568039364e1
Christian Mugione <chmugione@gmail.com>
2023-08-27 13:12:41 -0300

    q

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const database = "db.json";
+
+const saveData = (data) => {
+  fs.writeFileSync(database, JSON.stringify(data), "utf-8");
+};
+
+const loadData = () => {
+  try {
+    const jsonData = fs.readFileSync(database, "utf-8");
+    return JSON.parse(jsonData);
+  } catch (err) {
+    return {};
+  }
+};
 
 const app = express();
 const port = 8080;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hola viejo");
